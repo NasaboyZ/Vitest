a021d48e45f9fb5fd0db4622b6522a74f54afa31
NazaboyZ <josefleite.00@hotmail.com>
2025-03-02 11:36:01 +0100

    AAA standards

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,7 +2,15 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should summarize all number values in a array", () => {
-  const result = add([1, 2, 3]);
+  // Arrange
+  const numbers = [1, 2];
 
-  expect(result).toBe(6);
+  const expectedResult = numbers.reduce(
+    (preValue, curValue) => preValue + curValue,
+    0
+  );
+  //Act
+  const result = add(numbers);
+  // Assert
+  expect(result).toBe(expectedResult);
 });
