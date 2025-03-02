f0f86d7083b63c448d1a7c5db88acca600fe6071
NazaboyZ <josefleite.00@hotmail.com>
2025-03-02 11:53:13 +0100

    defining behaviors fixing errors in code

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -14,3 +14,27 @@ it("should summarize all number values in a array", () => {
   // Assert
   expect(result).toBe(expectedResult);
 });
+
+it("should yield NAN if a least one invalid number is provided", () => {
+  const inputs = ["invalid", 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  // Arrange
+  const numbers = ["1", "2"];
+
+  // Act
+  const result = add(numbers);
+
+  const expectedResult = numbers.reduce(
+    (preValue, curValue) => +preValue + +curValue,
+    0
+  );
+
+  //Assert
+  expect(result).toBe(expectedResult);
+});
